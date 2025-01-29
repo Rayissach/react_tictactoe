e2b7af2ec838c6b5f3555a6f677d9e0e882625da
Raymond Robinson <rayissach@gmail.com>
2025-01-28 21:19:01 -0500

    Edited the css for square component

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Square from './components/Square'
 import './App.css'
 
 function App() {
+  const [message, setMessage] = useState('Tic Tac Toe')
 
   return (
-    <div>
+    <div className='bg-[#392F5A] h-screen align-text-center'>
+      <h1 className='text-3xl bold text-[#A8C7BB] justify-self-center p-10'>{message}</h1>
       <Square />
     </div>
   )
--- app/src/components/Square.jsx
+++ app/src/components/Square.jsx
@@ -3,8 +3,10 @@ import React, { useState } from 'react'
 export default function Square() {
     return (
         <>
-            <div className>
-                <button className='square'>X</button>
+            <div className='flex items-center justify-center contain'>
+                <button className='flex items-center justify-center w-16 h-16 bg-gray-800 text-yellow-400 border border-gray-600 rounded-lg shadow-md hover:bg-gray-700 transition-all duration-200 active:scale-95'>
+                    <h1 className='text-lg/7 '>X</h1>
+                </button>
             </div>
         </>
     )
