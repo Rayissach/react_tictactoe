79a18cafb8d5094dbc203b48292cd43949d641e6
Raymond Robinson <rayissach@gmail.com>
2025-01-30 13:24:04 -0500

    Passing state hook as a prop value for Board component

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react'
-import Square from './components/Square'
+import Board from './components/Square'
 import './App.css'
 
 function App() {
   const [message, setMessage] = useState('Tic Tac Toe')
+  const [value, setValue] = useState('X')
+
 
   return (
     <div className='bg-[#392F5A] min-h-screen align-text-center'>
       <h1 className='text-3xl bold text-[#A8C7BB] justify-self-center p-10 mb-25'>{message}</h1>
       <div className='flex items-center justify-center'>
-        <Square />
+        <Board value={value}/>
       </div>
     </div>
   )
